fc319cee66b21c58356c1d4d49cb58371fb37f2e
Ben Orchard <thefirstmuffinman@gmail.com>
2021-03-16 14:15:23 +0000

    add tests and improve existing (for demonstration)

--- test/ExprSSA/ToSSASpec.hs
+++ test/ExprSSA/ToSSASpec.hs
@@ -16,11 +16,20 @@ lastStmt = SSAStmt "return"
 spec :: Spec
 spec = do
   describe "Expr -> SSA conversion" $ do
-    it "converts a plain value expression" $
+    it "converts a plain value expression: 42 == \"return := 42\"" $
       let expr = ENum 42
-          prog = lastStmt (SSAExprValue (SSAValueNum 42)) : []
+          prog = [ lastStmt (SSAExprValue (SSAValueNum 42)) ]
        in exprToSSA' expr `shouldBe` prog
-    it "converts a plain var expression" $
+    it "converts a plain var expression:    x == \"return :=  x\"" $
       let expr = EVar "x"
-          prog = lastStmt (SSAExprValue (SSAValueVar "x")) : []
+          prog = [ lastStmt (SSAExprValue (SSAValueVar "x")) ]
+       in exprToSSA' expr `shouldBe` prog
+    it "converts a depth-1 binop expression: x+1     == \"return := x+1\"" $
+      let expr = EBinOp Add (EVar "x") (ENum 1)
+          prog = [ lastStmt (SSAExprBinOp Add (SSAValueVar "x") (SSAValueNum 1)) ]
+       in exprToSSA' expr `shouldBe` prog
+    it "converts a depth-2 binop expression: (x+y)*2 == \"L0 := x+y; return = L0*2\"" $
+      let expr = EBinOp Mul (EBinOp Add (EVar "x") (EVar "y")) (ENum 2)
+          prog = [ SSAStmt "L0" (SSAExprBinOp Add (SSAValueVar "x") (SSAValueVar "y"))
+                 , lastStmt (SSAExprBinOp Mul (SSAValueVar "L0") (SSAValueNum 2)) ]
        in exprToSSA' expr `shouldBe` prog
